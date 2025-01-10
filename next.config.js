6df8b1d1c8dd3f115e33a4a2a2d59fd255d9867a
luxiaobin <luxiaobin@yeah.net>
2025-01-10 21:58:08 +0800

    modify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -13,9 +11,5 @@ const nextConfig = {
     ],
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 module.exports = nextConfig;
